ac6346ddabed349567987212ce67c2bdd3be8dd9
Jaanus Siim <jaanus@jaanussiim.com>
2019-03-27 18:37:18 +0200

    Fix Xcode 10.2 warning

--- Sources/NSManagedObject+EnityDefinition.swift
+++ Sources/NSManagedObject+EnityDefinition.swift
@@ -18,7 +18,7 @@ import Foundation
 import CoreData
 
 public extension NSManagedObject {
-    public class func entityName() -> String {
+    class func entityName() -> String {
         return NSStringFromClass(self).components(separatedBy: ".").last!
     }    
 }
