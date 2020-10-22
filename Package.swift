5f4c878fd49bec9c25fc8fd665a92a72eddd088d
Jaanus Siim <jaanus@jaanussiim.com>
2020-10-22 16:59:54 +0300

    Mark it dynamic

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CoreDataPersistence",
+            type: .dynamic,
             targets: ["CoreDataPersistence"]),
     ],
     dependencies: [
