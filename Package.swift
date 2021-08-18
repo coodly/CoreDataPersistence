ec6ef21f7e36f9738908e26eea5700d0f04de24c
Jaanus Siim <jaanus@jaanussiim.com>
2021-08-18 07:19:24 +0300

    Mark macos platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CoreDataPersistence",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
